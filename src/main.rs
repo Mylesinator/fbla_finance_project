bb24219daa33ef59528df7f55bb3794914786d4a
Nooneyouknowof <octaviomcnaughton@gmail.com>
2024-12-06 17:59:48 -0700

    First working Window!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 slint::slint!{
+    import { Button } from "std-widgets.slint";
     export component App inherits Window {
         Text { text: "Hello World!"; }
+        Button { text: "Click"; }
     }
 }
 
 fn main() {
+    App::new().unwrap().run().unwrap();
     println!("Hello, world!");
 }
