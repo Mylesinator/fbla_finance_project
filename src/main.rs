b9216064fbba5c1a6f636b6fdbf81cd38d63837e
Nooneyouknowof <octaviomcnaughton@gmail.com>
2024-12-11 14:06:26 -0700

    Added styling to login and added some functionality

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,55 @@
+use slint::{PhysicalSize, WindowSize};
+
 slint::slint! {
     import { HorizontalBox, VerticalBox, LineEdit, Button } from "std-widgets.slint";
 
-    export component App inherits Window {
-        in-out property <int> test: 0;
+    export component Login inherits Window {
+        title: "FBLA Finance Login";
+        always-on-top: false;
+        background: black;
+        out property <string> username;
+        in-out property <string> password;
         callback login-pressed <=> login.clicked;
         VerticalBox {
             padding: 10rem;
-            username := LineEdit {
+            LineEdit {
+                input-type: text;
                 placeholder-text: "username";
+                padding-left: 2rem;
+                font-size: 3rem;
+                edited(text) => {username = text}
             }
-            password := LineEdit {
+            LineEdit {
                 input-type: InputType.password;
                 placeholder-text: "password";
+                font-size: 3rem;
+                edited(text) => {password = text}
             }
             login := Button {
-                text: "Login " + root.test;
+                text: "Login";
+            }
+            Button {
+                accessible-role: AccessibleRole.none;
+                primary: true;
+                text: "Button";
             }
         }
     }
 }
 
 fn main() {
-    let app = App::new().unwrap();
-    let app_weak = app.as_weak();
-    app.on_login_pressed(move || {
-        let app = app_weak.upgrade().unwrap();
-        let mut value = app.get_test();
-        value += 1;
-        app.set_test(value);
-        println!("{value}");
+    let login = Login::new().unwrap();
+    login.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
+    let login_weak = login.as_weak();
+    login.on_login_pressed(move || {
+        let login = login_weak.upgrade().unwrap();
+        let username = login.get_username();
+        let password = login.get_password();
+        if username != "" || password != "" {
+            println!("Login form: {} & {}", username, password);
+        }
     });
     
-    app.run().unwrap();
+    login.run().unwrap();
     println!("Programmed Ended!");
 }
