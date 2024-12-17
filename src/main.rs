5b63898a4f8b463efeb28e0ca88c7c42da494c86
Nooneyouknowof <ocatviomcnaughton@gmail.com>
2024-12-16 22:37:31 -0700

    Added stuff

--- src/main.rs
+++ src/main.rs
@@ -21,28 +21,55 @@ fn main() {
     let signup_weak = signup.as_weak();
     // let home_weak = home.as_weak();
 
-    signup.on_login_pressed(move || {
-        // let signup = signup_weak.upgrade().unwrap();
-        println!("Sign up!");
+    signup.on_signup_pressed({
+        // let signup_weak = signup_weak.clone();
+        move || {
+            // let signup = signup_weak.upgrade().unwrap();
+            // println!("{}", signup);
+            println!("Sign up!");
+        }
     });
 
-    login.on_signup_pressed(move || {
-        let login = login_weak.upgrade().unwrap();
-        signup.set_username(login.get_username());
-        signup.set_password(login.get_password());
-        handle_visibility(|| signup.show());
-        handle_visibility(|| login.hide());
+    signup.on_login_pressed({
+        let signup_weak = signup_weak.clone();
+        let login_weak = login_weak.clone();
+        move || {
+            let signup = signup_weak.upgrade().unwrap();
+            let login = login_weak.upgrade().unwrap();
+            let window_position = login.window().position();
+            handle_visibility(|| login.show());
+            handle_visibility(|| signup.hide());
+            signup.window().set_position(window_position);
+        }
     });
 
-    login.on_login_pressed(move || {
-        let login = login_weak.upgrade().unwrap();
-        let username = login.get_username();
-        let password = login.get_password();
-        if username != "" || password != "" {
-            println!("Login form: {} & {}", username, password);
-            home.set_username(username);
-            handle_visibility(|| home.show());
+    login.on_signup_pressed({
+        let login_weak = login_weak.clone();
+        let signup_weak = signup_weak.clone();
+        move || {
+            let login = login_weak.upgrade().unwrap();
+            let signup = signup_weak.upgrade().unwrap();
+            signup.set_username(login.get_username());
+            signup.set_password(login.get_password());
+            let window_position = signup.window().position();
+            handle_visibility(|| signup.show());
             handle_visibility(|| login.hide());
+            login.window().set_position(window_position);
+        }
+    });
+
+    login.on_login_pressed({
+        let login_weak = login_weak.clone();
+        move || {
+            let login = login_weak.upgrade().unwrap();
+            let username = login.get_username();
+            let password = login.get_password();
+            if username != "" || password != "" {
+                println!("Login form: {} & {}", username, password);
+                home.set_username(username);
+                handle_visibility(|| home.show());
+                handle_visibility(|| login.hide());
+            }
         }
     });
     
