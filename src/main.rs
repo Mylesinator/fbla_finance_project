5301c085cd6b33b4f72e8779a35401e6a8075409
Nooneyouknowof <octaviomcnaughton@gmail.com>
2024-12-16 20:37:40 -0700

    Moved slint code to be a component

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,9 @@
+slint::include_modules!();
 use slint::{PhysicalSize, WindowSize};
-
-slint::slint! {
-    import { HorizontalBox, VerticalBox, LineEdit, Button } from "std-widgets.slint";
-
-    export component Login inherits Window {
-        title: "FBLA Finance Login";
-        always-on-top: false;
-        background: black;
-        out property <string> username;
-        in-out property <string> password;
-        callback login-pressed <=> login.clicked;
-        VerticalBox {
-            padding: 10rem;
-            LineEdit {
-                input-type: text;
-                placeholder-text: "username";
-                padding-left: 2rem;
-                font-size: 3rem;
-                edited(text) => {username = text}
-            }
-            LineEdit {
-                input-type: InputType.password;
-                placeholder-text: "password";
-                font-size: 3rem;
-                edited(text) => {password = text}
-            }
-            login := Button {
-                text: "Login";
-            }
-            Button {
-                accessible-role: AccessibleRole.none;
-                primary: true;
-                text: "Button";
-            }
-        }
-    }
-}
 
 fn main() {
     let login = Login::new().unwrap();
+    let home = Home::new().unwrap();
     login.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
     let login_weak = login.as_weak();
     login.on_login_pressed(move || {
@@ -47,6 +12,9 @@ fn main() {
         let password = login.get_password();
         if username != "" || password != "" {
             println!("Login form: {} & {}", username, password);
+            Window::close;
+            home.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
+            home.run().unwrap();
         }
     });
     
