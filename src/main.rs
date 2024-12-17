c9940f51b7b3a173adc5778b0ca616aa4bbb91e6
LazyKotaa <mylesmarshall06@gmail.com>
2024-12-17 03:45:13 -0700

    window starts maximized

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() -> Result<(), PlatformError> {
     let home = Home::new().unwrap();
 
     home.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
+    home.window().set_maximized(true);
 
     return home.run();
 }
