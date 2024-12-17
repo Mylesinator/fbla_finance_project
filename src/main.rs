f2a4f27dcaff87a0e25638f4fac7639422112e14
Nooneyouknowof <ocatviomcnaughton@gmail.com>
2024-12-16 21:59:36 -0700

    Added some functionality

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,39 @@ where
 
 fn main() {
     let login = Login::new().unwrap();
+    let signup = SignUp::new().unwrap();
     let home = Home::new().unwrap();
     login.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
+    signup.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
     home.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
     let login_weak = login.as_weak();
+    let signup_weak = signup.as_weak();
+    // let home_weak = home.as_weak();
+
+    signup.on_login_pressed(move || {
+        // let signup = signup_weak.upgrade().unwrap();
+        println!("Sign up!");
+    });
+
+    login.on_signup_pressed(move || {
+        let login = login_weak.upgrade().unwrap();
+        signup.set_username(login.get_username());
+        signup.set_password(login.get_password());
+        handle_visibility(|| signup.show());
+        handle_visibility(|| login.hide());
+    });
+
     login.on_login_pressed(move || {
         let login = login_weak.upgrade().unwrap();
         let username = login.get_username();
         let password = login.get_password();
         if username != "" || password != "" {
             println!("Login form: {} & {}", username, password);
+            home.set_username(username);
             handle_visibility(|| home.show());
             handle_visibility(|| login.hide());
         }
     });
-
-    // let home_weak = 
     
     login.run().unwrap();
     println!("Programmed Ended!");
