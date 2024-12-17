6d814322c5b3d9eecd6e0d625f3c9dbe375d7f66
Nooneyouknowof <ocatviomcnaughton@gmail.com>
2024-12-16 21:07:54 -0700

    Fixed window switching and added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 slint::include_modules!();
-use slint::{PhysicalSize, WindowSize};
+use slint::{PhysicalSize, WindowSize, PlatformError, ComponentHandle};
+
+fn handle_visibility<F>(action: F)
+where
+    F: FnOnce() -> Result<(), PlatformError>,
+{
+    if let Err(e) = action() {
+        eprintln!("Error changing visibility: {}", e);
+    }
+}
 
 fn main() {
     let login = Login::new().unwrap();
     let home = Home::new().unwrap();
     login.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
+    home.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
     let login_weak = login.as_weak();
     login.on_login_pressed(move || {
         let login = login_weak.upgrade().unwrap();
@@ -12,11 +22,12 @@ fn main() {
         let password = login.get_password();
         if username != "" || password != "" {
             println!("Login form: {} & {}", username, password);
-            Window::close;
-            home.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
-            home.run().unwrap();
+            handle_visibility(|| home.show());
+            handle_visibility(|| login.hide());
         }
     });
+
+    // let home_weak = 
     
     login.run().unwrap();
     println!("Programmed Ended!");
