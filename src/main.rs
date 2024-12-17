a2b5bbc4532ad526fb5c1a8eb90ab25d73a4d4f3
Nooneyouknowof <octaviomcnaughton@gmail.com>
2024-12-17 11:33:34 -0700

    Updated styling and added a few quality of life features

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,20 @@ slint::include_modules!();
 
 use slint::{ComponentHandle, PhysicalSize, PlatformError, Window, WindowSize};
 
-fn handle_visibility<F>(action: F)
-where
-    F: FnOnce() -> Result<(), PlatformError>,
-{
-    if let Err(e) = action() {
-        eprintln!("Error changing visibility: {}", e);
-    }
-}
-
 fn switch_window(current_window: &Window, next_window: &Window) -> Result<(), PlatformError> {
     let window_position = current_window.position();
     
-    // fixes the issue where it unmaximizes the window
-    // i assume you're not supposed to reposition it when maximized cause it's stretched to the screen
     if !current_window.is_maximized() {
         next_window.set_position(window_position);
     }
 
     next_window.set_size(current_window.size());
     next_window.set_maximized(current_window.is_maximized());
-    
-    // magical question mark.....
+    next_window.set_fullscreen(current_window.is_fullscreen());
+
     next_window.show()?;
     current_window.hide()?;
 
-    // handle_visibility(|| next_window.show());
-    // handle_visibility(|| current_window.hide());
-
-    // returning an Ok() somehow fixes the problem with the return data type
-    // now the magical question mark works
     return Ok(());
 }
 
@@ -43,35 +27,46 @@ fn main() {
     // only the starting window needs to have this size considering it takes the previous window size when you switch
     login.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
 
-    // signup.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
-    // home.window().set_size(WindowSize::Physical(PhysicalSize::new(800, 600)));
-
     let login_weak = login.as_weak();
     let signup_weak = signup.as_weak();
 
     // let home_weak = home.as_weak();
 
     signup.on_signup_pressed({
-        // let signup_weak = signup_weak.clone();
+        let signup_weak = signup_weak.clone();
+        let login_weak = login_weak.clone();
         move || {
-            // let signup = signup_weak.upgrade().unwrap();
-            // println!("{}", signup);
-            println!("Sign up!");
+            let signup = signup_weak.upgrade().unwrap();
+            let login = login_weak.upgrade().unwrap();
+            let username = signup.get_username();
+            let password = signup.get_password();
+            println!("attempted to Signed up as {username} w/ {password}");
+            if username != "" && password != "" {
+                signup.set_username("".into());
+                signup.set_password("".into());
+                let _ = switch_window(signup.window(), login.window());
+            }
         }
     });
 
     signup.on_login_pressed({
         let signup = signup_weak.clone().upgrade().unwrap();
         let login = login_weak.clone().upgrade().unwrap();
-
-        // let _ = : used for ignoring the returned result :I
-        move || { let _ = switch_window(signup.window(), login.window()); }
+        move || {
+            login.set_username(signup.get_username());
+            login.set_password("".into());
+            let _ = switch_window(signup.window(), login.window());
+        }
     });
 
     login.on_signup_pressed({
         let signup = signup_weak.clone().upgrade().unwrap();
         let login = login_weak.clone().upgrade().unwrap();
-        move || { let _ = switch_window(login.window(), signup.window()); }
+        move || {
+            signup.set_username(login.get_username());
+            signup.set_password(login.get_password());
+            let _ = switch_window(login.window(), signup.window());
+        }
     });
 
     login.on_login_pressed({
@@ -80,7 +75,7 @@ fn main() {
             let username = login.get_username();
             let password = login.get_password();
 
-            if username != "" || password != "" {
+            if username != "" && password != "" {
                 println!("Login form: {} & {}", username, password);
 
                 let _ = switch_window(login.window(), home.window());
